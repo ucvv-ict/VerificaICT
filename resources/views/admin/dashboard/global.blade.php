30f77d7ddd3984001b6f502cf5cbf96e3f6deaec
ucvv-ict <assistenza.ict@ucvv.it>
2026-02-12 12:32:53 +0100

    feat: add compliance progress bar to global dashboard

--- resources/views/admin/dashboard/global.blade.php
+++ resources/views/admin/dashboard/global.blade.php
@@ -11,6 +11,7 @@
         <thead class="bg-gray-100">
             <tr>
                 <th class="text-left p-3">Ente</th>
+                <th class="p-3 text-center">Conformità</th>
                 <th class="p-3 text-center">🔴 Critici</th>
                 <th class="p-3 text-center">🟡 Warning</th>
                 <th class="p-3 text-center">🟢 OK</th>
@@ -22,6 +23,33 @@
             <tr class="border-t">
                 <td class="p-3 font-semibold">
                     {{ $row['entity']->nome }}
+
+                    @if($row['critici'] > 0)
+                        <span class="ml-2 px-2 py-1 text-xs bg-red-100 text-red-700 rounded">
+                            Criticità
+                        </span>
+                    @elseif($row['warning'] > 0)
+                        <span class="ml-2 px-2 py-1 text-xs bg-yellow-100 text-yellow-700 rounded">
+                            Attenzione
+                        </span>
+                    @else
+                        <span class="ml-2 px-2 py-1 text-xs bg-green-100 text-green-700 rounded">
+                            OK
+                        </span>
+                    @endif
+                </td>
+
+                <td class="p-3 text-center">
+                    <div class="w-full bg-gray-200 rounded h-3">
+                        <div class="h-3 rounded
+                            {{ $row['percentuale'] >= 80 ? 'bg-green-500' :
+                            ($row['percentuale'] >= 50 ? 'bg-yellow-500' : 'bg-red-500') }}"
+                            style="width: {{ $row['percentuale'] }}%">
+                        </div>
+                    </div>
+                    <div class="text-xs mt-1">
+                        {{ $row['percentuale'] }}%
+                    </div>
                 </td>
 
                 <td class="p-3 text-center">
