86e14a56784b1a3b8372e5f7e267fdca1baa9c81
ucvv-ict <assistenza.ict@ucvv.it>
2026-02-12 11:15:54 +0100

    feat: add operator dashboard controller

---
+++ app/Http/Controllers/OperatorDashboardController.php
@@ -0,0 +1,26 @@
+<?php
+namespace App\Http\Controllers;
+
+use App\Models\EntitySecurityTask;
+use Illuminate\Http\Request;
+
+class OperatorDashboardController extends Controller
+{
+    public function index(Request $request)
+    {
+        $user = auth()->user();
+
+        $entityIds = $user->entities()->pluck('entities.id');
+
+        $tasks = EntitySecurityTask::with([
+                'entity',
+                'securityTask',
+                'lastCheck'
+            ])
+            ->whereIn('entity_id', $entityIds)
+            ->where('attiva', true)
+            ->get();
+
+        return view('operator.dashboard', compact('tasks'));
+    }
+}
