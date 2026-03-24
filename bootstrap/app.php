89148ef98d8236cb693217d72f6d1a608e5c0320
ucvv-ict <assistenza.ict@ucvv.it>
2026-03-24 12:55:06 +0100

    Modiifcato bootstrap app.php tornato indietro

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -7,7 +7,6 @@ use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
-    ->withEnvironmentVariables() // 👈 AGGIUNGI QUESTA RIGA
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
@@ -24,5 +23,4 @@ return Application::configure(basePath: dirname(__DIR__))
     ])
     ->withExceptions(function (Exceptions $exceptions): void {
         //
-    })
-    ->create();
+    })->create();
